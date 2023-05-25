3d5d77c97f60a79e0791a4720aea2c87a93eb4fd
ChrisRDavila <crossdavila@gmail.com>
2023-05-25 11:24:58 -0700

    WIP. save point before databse update

---
+++ BestRestaurants/Migrations/20230525181732_RestaurantLocationWithRequire.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BestRestaurants.Migrations
+{
+    public partial class RestaurantLocationWithRequire : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Restaurants",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Restaurants");
+        }
+    }
+}
